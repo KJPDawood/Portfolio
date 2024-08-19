72945f3fbfc13374d81898dfe6d17cccf73ade9b
KJPDawood <kjpdawood@gmail.com>
2024-08-19 07:08:26 -0700

    about half done

--- files/about.js
+++ files/about.js
@@ -15,14 +15,14 @@ document.querySelector('.prev').addEventListener('click', () => {
 
 function updateCarousel() {
     carousel.style.transform = `translateX(${-index * 100}%)`;
-};
+}; 
 const color = document.getElementById("color");
 const colors = document.getElementById("colors");
-const color1 = document.getElementById("red");
-const color2 = document.getElementById("green");
-const color3 = document.getElementById("sky-blue");
-const color4 = document.getElementById("yellow");
-const color5 = document.getElementById("orange");
+const color1 = document.getElementById("red"); 
+const color2 = document.getElementById("green"); 
+const color3 = document.getElementById("sky-blue"); 
+const color4 = document.getElementById("yellow"); 
+const color5 = document.getElementById("orange"); 
 const root = document.documentElement;
 document.addEventListener("DOMContentLoaded", function () {
     function toggleDiv2() {
@@ -39,30 +39,25 @@ document.addEventListener("DOMContentLoaded", function () {
 color1.addEventListener("click",function () {
     root.style.setProperty("--gradientColor" ,"black,rgb(255, 0, 0),red,black,black");
     root.style.setProperty("--textColor" ,"red");
-    root.style.setProperty("--boxShadow" ,"0 0 15px 0px rgba(255, 0, 0)");
-    root.style.setProperty("--imgColor" ," black,red,black,red,black,red,black");
+    root.style.setProperty("--boxShadow" ,"0 0 15px rgba(255, 0, 0)");
 });
 color2.addEventListener("click",function () {
     root.style.setProperty("--gradientColor" ,"black,rgb(0, 255, 0),rgb(0, 255, 0),black,black");
     root.style.setProperty("--textColor" ,"rgb(0, 255, 0)");
-    root.style.setProperty("--boxShadow" ,"0 0 15px 0px rgb(0, 255, 0)");
-    root.style.setProperty("--imgColor" ," black,rgb(0, 255, 0),black,rgb(0, 255, 0),black,rgb(0, 255, 0),black");
+    root.style.setProperty("--boxShadow" ,"0 0 15px rgb(0, 255, 0)");
 });
 color3.addEventListener("click",function () {
     root.style.setProperty("--gradientColor" ,"black,rgb(0, 174, 255),rgb(0, 174, 255),black,black");
     root.style.setProperty("--textColor" ,"rgb(0, 174, 255)");
-    root.style.setProperty("--boxShadow" ,"0 0 15px 0px rgb(0, 174, 255)");
-    root.style.setProperty("--imgColor" ," black,rgb(0, 174, 255),black,rgb(0, 174, 255),black,rgb(0, 174, 255),black");
+    root.style.setProperty("--boxShadow" ,"0 0 15px rgb(0, 174, 255)");
 });
 color4.addEventListener("click",function () {
     root.style.setProperty("--gradientColor" ,"black,rgb(255, 255, 0),rgb(255, 255, 0),black,black");
     root.style.setProperty("--textColor" ,"rgb(255, 255, 0)");
-    root.style.setProperty("--boxShadow" ,"0 0 15px 0px rgb(255, 255, 0)");
-    root.style.setProperty("--imgColor" ," black,rgb(255, 255, 0),black,rgb(255, 255, 0),black,rgb(255, 255, 0),black");
+    root.style.setProperty("--boxShadow" ,"0 0 15px rgb(255, 255, 0)");
 });
 color5.addEventListener("click",function () {
     root.style.setProperty("--gradientColor" ,"black,rgb(255, 166, 0),rgb(255, 166, 0),black,black");
     root.style.setProperty("--textColor" ,"rgb(255, 166, 0)");
-    root.style.setProperty("--boxShadow" ,"0 0 15px 0px rgrgb(255, 166, 0)");
-    root.style.setProperty("--imgColor" ," black,rgb(255, 166, 0),black,rgb(255, 166, 0),black,rgb(255, 166, 0),black");
+    root.style.setProperty("--boxShadow" ,"0 0 15px rgb(255, 166, 0)");
 });
